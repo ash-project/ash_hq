9ac964694f453601f9dccf3bc1b895a7ff836d58
Zach Daniel <zach@zachdaniel.dev>
2023-01-15 01:48:24 -0500

    improvement: handle optional arguments

--- lib/ash_hq/docs/resources/dsl/dsl.ex
+++ lib/ash_hq/docs/resources/dsl/dsl.ex
@@ -33,6 +33,10 @@ defmodule AshHq.Docs.Dsl do
 
     attribute :examples, {:array, :string}
     attribute :args, {:array, :string}
+    attribute :optional_args, {:array, :string} do
+      default []
+    end
+    attribute :arg_defaults, :map
     attribute :path, {:array, :string}
     attribute :recursive_as, :string
     attribute :order, :integer, allow_nil?: false
@@ -86,6 +90,8 @@ defmodule AshHq.Docs.Dsl do
     references do
       reference :library_version, on_delete: :delete
     end
+
+    migration_defaults optional_args: "[]"
   end
 
   actions do
