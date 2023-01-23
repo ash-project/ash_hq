3e96b02fe3285405b63f9d4e5e43fcea9faa7acc
Rebecca Le <543859+sevenseacat@users.noreply.github.com>
2023-01-23 11:17:23 +0800

    fix: Only start Nostrum when the Discord bot is enabled (#73)

--- lib/ash_hq/application.ex
+++ lib/ash_hq/application.ex
@@ -18,6 +18,7 @@ defmodule AshHq.Application do
 
     discord_bot =
       if Application.get_env(:ash_hq, :discord_bot) do
+        Nostrum.Application.start(:normal, [])
         [AshHq.Discord.Supervisor]
       else
         []
