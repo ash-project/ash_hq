013a075b2408446e1fe8a8e5ac855eae3bff0cb0
Zach Daniel <zach@zachdaniel.dev>
2023-02-01 10:25:21 -0500

    chore: credo

--- lib/ash_hq/docs/extensions/search/preparations/deselect_searchable.ex
+++ lib/ash_hq/docs/extensions/search/preparations/deselect_searchable.ex
@@ -1,4 +1,5 @@
 defmodule AshHq.Docs.Extensions.Search.Preparations.DeselectSearchable do
+  @moduledoc "Deselects the searchable attribute"
   use Ash.Resource.Preparation
 
   def prepare(query, _, _) do
