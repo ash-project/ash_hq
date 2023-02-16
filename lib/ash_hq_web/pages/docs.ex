7789755b9b8253c6da08d34676ba351168305cd7
Zach Daniel <zach@zachdaniel.dev>
2023-02-16 07:19:54 -0500

    fix: don't link to dsls instead of modules

--- lib/ash_hq_web/pages/docs.ex
+++ lib/ash_hq_web/pages/docs.ex
@@ -912,35 +912,7 @@ defmodule AshHqWeb.Pages.Docs do
         )
       end
     else
-      if socket.assigns.library_version && socket.assigns[:params]["module"] do
-        extension =
-          Enum.find(socket.assigns.library_version.extensions, fn extension ->
-            extension.sanitized_name == socket.assigns[:params]["module"] ||
-              AshHqWeb.DocRoutes.sanitize_name(extension.target) ==
-                socket.assigns[:params]["module"]
-          end)
-
-        extension =
-          if extension do
-            dsls_query =
-              AshHq.Docs.Dsl
-              |> Ash.Query.sort(order: :asc)
-              |> load_for_search()
-
-            options_query =
-              AshHq.Docs.Option
-              |> Ash.Query.sort(order: :asc)
-              |> load_for_search()
-
-            AshHq.Docs.load!(extension, dsls: dsls_query, options: options_query)
-          end
-
-        assign(socket,
-          extension: extension
-        )
-      else
-        assign(socket, :extension, nil)
-      end
+      assign(socket, :extension, nil)
     end
   end
 
