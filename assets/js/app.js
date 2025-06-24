ac67e4db346facbd6e307a8dc2976653bc3872b5
Rebecca Le <rebecca@alembic.com.au>
2025-06-24 14:54:00 +0800

    Fix Persistence example on homepage
    The attribute names should be the same between the code and the PostgreSQL table

--- assets/js/app.js
+++ assets/js/app.js
@@ -1094,7 +1094,7 @@ end`,
     attribute :title, :string,
       allow_nil?: false
     attribute :content, :string
-    attribute :state, :atom,
+    attribute :status, :atom,
       constraints: [
         one_of: [:draft, :published]
       ]
